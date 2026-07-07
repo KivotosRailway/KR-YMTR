b7a53cfe9cfd0412631979200702fa5c907df0ea
yomi_307 <yomi2023@qq.com>
2026-07-07 17:50:54 +0800

    手动驾驶速度调节优化

--- fabric/src/main/java/mtr/mappings/FabricRegistryUtilities.java
+++ fabric/src/main/java/mtr/mappings/FabricRegistryUtilities.java
@@ -22,17 +22,11 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 public interface FabricRegistryUtilities {
-
-	Map<ResourceLocation, List<Item>> CONNECTOR_CREATIVE_TAB_ITEMS = new LinkedHashMap<>();
-	boolean[] connectorRegistered = {false};
 
 	static void registerItemModelPredicate(String id, Item item, String tag) {
 		FabricModelPredicateProviderRegistry.register(item, new ResourceLocation(id), (itemStack, clientWorld, livingEntity, i) -> itemStack.getOrCreateTag().contains(tag) ? 1 : 0);
@@ -54,11 +48,6 @@ public interface FabricRegistryUtilities {
 		ItemGroupEvents.MODIFY_ENTRIES_ALL.register((tab, entries) -> {
 			if (tab == creativeModeTab) entries.accept(item);
 		});
-
-		final ResourceLocation tabKey = BuiltInRegistries.CREATIVE_MODE_TAB.getKey(creativeModeTab);
-		if (tabKey != null) {
-			CONNECTOR_CREATIVE_TAB_ITEMS.computeIfAbsent(tabKey, k -> new ArrayList<>()).add(item);
-		}
 	}
 
 	static CreativeModeTab createCreativeModeTab(ResourceLocation id, Supplier<ItemStack> supplier) {
@@ -70,58 +59,5 @@ public interface FabricRegistryUtilities {
 				.build();
 		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, id, tab);
 		return tab;
-	}
-
-	static void registerConnectorCreativeTabsFallback() {
-		if (connectorRegistered[0]) {
-			return;
-		}
-		connectorRegistered[0] = true;
-
-		try {
-			Class<?> mcForgeClass = Class.forName("net.minecraftforge.common.MinecraftForge");
-			Field eventBusField = mcForgeClass.getField("EVENT_BUS");
-			Object eventBus = eventBusField.get(null);
-
-			Method addListenerMethod = null;
-			for (Method m : eventBus.getClass().getMethods()) {
-				if (m.getName().equals("addListener") && m.getParameterCount() == 1
-						&& m.getParameterTypes()[0] == Consumer.class) {
-					addListenerMethod = m;
-					break;
-				}
-			}
-
-			if (addListenerMethod == null) {
-				return;
-			}
-
-			Consumer<Object> handler = eventObj -> {
-				try {
-					Method getTab = eventObj.getClass().getMethod("getTab");
-					Method getEntries = eventObj.getClass().getMethod("getEntries");
-
-					CreativeModeTab tab = (CreativeModeTab) getTab.invoke(eventObj);
-					CreativeModeTab.Output output = (CreativeModeTab.Output) getEntries.invoke(eventObj);
-
-					ResourceLocation tabKey = BuiltInRegistries.CREATIVE_MODE_TAB.getKey(tab);
-					if (tabKey != null) {
-						List<Item> items = CONNECTOR_CREATIVE_TAB_ITEMS.get(tabKey);
-						if (items != null) {
-							for (Item item : items) {
-								output.accept(new ItemStack(item));
-							}
-						}
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			};
-
-			addListenerMethod.invoke(eventBus, handler);
-		} catch (ClassNotFoundException e) {
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 }
