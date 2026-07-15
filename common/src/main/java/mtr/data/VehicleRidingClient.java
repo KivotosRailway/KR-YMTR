c077b2fbe4a3356d1c7ffabf56467c2d4548a2f8
yomi_307 <yomi2023@qq.com>
2026-07-15 19:11:14 +0800

    3.5.9

--- common/src/main/java/mtr/data/VehicleRidingClient.java
+++ common/src/main/java/mtr/data/VehicleRidingClient.java
@@ -31,6 +31,11 @@ public class VehicleRidingClient {
 	private float lastSentTicks;
 	private int interval;
 	private int previousInterval;
+	private double prevMoveX;
+	private double prevMoveY;
+	private double prevMoveZ;
+	private float prevPlayerYRot;
+	private boolean hasPrevPosition;
 
 	private final List<Double> offset = new ArrayList<>();
 	private final Map<UUID, Float> percentagesX = new HashMap<>();
@@ -146,6 +151,15 @@ public class VehicleRidingClient {
 				clientPlayer.setSpeed(0);
 				if (MTRClient.getGameTick() > 40) {
 					clientPlayer.setPos(moveX, moveY, moveZ);
+					if (hasPrevPosition) {
+						clientPlayer.xo = prevMoveX;
+						clientPlayer.yo = prevMoveY;
+						clientPlayer.zo = prevMoveZ;
+					}
+					prevMoveX = moveX;
+					prevMoveY = moveY;
+					prevMoveZ = moveZ;
+					hasPrevPosition = true;
 				}
 			}
 
@@ -153,6 +167,7 @@ public class VehicleRidingClient {
 
 			if (shouldSetOffset) {
 				if (shouldSetYaw) {
+					final float playerYawBefore = Utilities.getYaw(clientPlayer);
 					float angleDifference = (float) Math.toDegrees(clientPrevYaw - yaw);
 					if (angleDifference > 180) {
 						angleDifference -= 360;
@@ -160,6 +175,7 @@ public class VehicleRidingClient {
 						angleDifference += 360;
 					}
 					Utilities.incrementYaw(clientPlayer, angleDifference);
+					clientPlayer.yRotO = playerYawBefore;
 				}
 				offset.add(x);
 				offset.add(y);
